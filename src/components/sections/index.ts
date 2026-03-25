c9cc6bca8e61263047191e2f097ac18729bf627b
yoursandeshshrestha <yoursandeshshrestha@gmail.com>
2026-03-26 00:11:32 +0530

    chore(client): remove old component structure

--- src/components/sections/index.ts
+++
@@ -1,7 +0,0 @@
-export { HeroSection } from './HeroSection';
-export { HowItWorksSection } from './HowItWorksSection';
-export { ServiceCardsSection } from './ServiceCardsSection';
-export { StatsBarSection } from './StatsBarSection';
-export { SocialProofSection } from './SocialProofSection';
-export { FAQSection } from './FAQSection';
-export { FinalCTASection } from './FinalCTASection';
