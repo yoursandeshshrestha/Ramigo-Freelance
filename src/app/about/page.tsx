1521a83c88f9796cdc55b86b8abc165f95f1cef6
yoursandeshshrestha <yoursandeshshrestha@gmail.com>
2026-03-26 00:10:45 +0530

    feat(client): add about page with team section and principles

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { AboutPageClient } from './AboutPageClient';
+
+export const metadata = {
+  title: 'About Remigo | FCA Regulated Mortgage Broker',
+  description: 'Meet the team behind Remigo. FCA regulated mortgage brokers with 15+ years experience. We help thousands of people find better mortgage deals every year.',
+  keywords: 'about remigo, mortgage broker, FCA regulated, mortgage advice',
+};
+
+export default function AboutPage() {
+  return <AboutPageClient />;
+}
